29bd24127b894666183140e07139179abe1fc85d
majiidmuhammad <majiid831@gmail.com>
2023-03-07 14:13:00 +0700

    perbaikan petugas

--- app/Http/Controllers/Admin/PetugasController.php
+++ app/Http/Controllers/Admin/PetugasController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Petugas;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class PetugasController extends Controller
 {
@@ -23,7 +25,35 @@ class PetugasController extends Controller
 
     public function store(Request $request)
     {
-        # code...
+        $data = $request->all();
+
+        $validate = Validator::make($data, [
+            'nama_petugas' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'unique:petugas'],
+            'password' => ['required', 'string', 'min:6'],
+            'telp' => ['required'],
+            'level' => ['required', 'in:admin,petugas'],
+        ]);
+
+        if ($validate->fails()){
+            return redirect()->back()->withErrors($validate);
+        }
+
+        $username = petugas::where('username', $data['username'])->first();
+
+        if ($username) {
+            return redirect()->back()->with(['username' => 'Username sudah digunakan']);
+        }
+
+        Petugas::create([
+            'nama_petugas' => $data['nama_petugas'],
+            'username' => $data['username'],
+            'password' => Hash::make($data['password']),
+            'telp' => $data['telp'],
+            'level' => $data['level'],
+        ]);
+
+        return redirect()->route('petugas.index');
     }
 
     public function edit($id_petugas)
